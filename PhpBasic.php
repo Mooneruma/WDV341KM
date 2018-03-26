149c8b48b588cd71fb47e0bd861748daffc9ca56
Mooneruma <31321473+Mooneruma@users.noreply.github.com>
2018-03-26 15:07:12 -0600

    Add files via upload

---
+++ PhpBasic.php
@@ -0,0 +1,21 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php 
+	$yourName = "Kyle Moon";
+	echo '<h1>PHP Basics</h1>';
+	echo '<h2>' . $yourName . '</h2>';
+	
+	$number1 = 10;
+	$number2 = 20;
+	$sum_total = $number1 + $number2;
+	
+	echo 'number1 is ' . $number1 . '<br>';
+	echo 'number2 is ' . $number2 . '<br>';
+	echo $sum_total;
+	
+ ?> 
+ </body>
+</html>
